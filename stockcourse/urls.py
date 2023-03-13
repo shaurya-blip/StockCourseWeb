862f9d2610417f602be28df65a980d515282adc0
SblipDev <shaurya.p.singh21@gmail.com>
2023-03-13 04:34:04 +0000

    Added core app to settings and IndexView to url

--- stockcourse/urls.py
+++ stockcourse/urls.py
@@ -15,7 +15,9 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from core.views import IndexView
 
 urlpatterns = [
     path("admin/", admin.site.urls),
+    path('', IndexView.as_view())
 ]
